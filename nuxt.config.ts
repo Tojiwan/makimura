977ee067349ddef5efce01ccc8fa1dfad3dc95bf
Tojiwan <hithug9909@gmail.com>
2025-03-13 13:22:37 +0800

    Added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vesp/nuxt-fontawesome',
+    '@nuxtjs/google-fonts'
+  ],
+
+  fontawesome: {
+    icons: {
+      solid: ['dollar-sign', 'cog', 'circle', 'check', 'calendar'],
+      regular: ['user']
+    }
+  }
 })
