972be5255ba3a234c573683c4cae18b9395eb216
REYVIC-DEV <joshuavreyes.dev@gmail.com>
2025-03-14 11:59:50 +0800

    fixed add and minus button added animation

--- components/OrderAdd.vue
+++ components/OrderAdd.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>
-        <button v-if="order.some(item => meal.name in item)" @click="decreaseOrder(meal.name)"
-            class="bg-gray-300 rounded-full w-8 h-8 flex items-center justify-center">
+    <div @click="countClicked()" class="rounded-full flex items-center justify-center transition-all duration-300 cursor-pointer h-10"
+        :class="[order.some(item => meal.name in item) ? 'border rounded-full ' : '', showBorder ? 'bg-white w-[100px]' : 'bg-[#EE2737] text-white w-10']">
+        <button v-if="showBorder" @click="decreaseOrder(meal.name)"
+            class="bg-gray-300 rounded-full w-full h-full flex items-center justify-center">
             <font-awesome icon="fa-solid fa-minus" style="color: #000;" />
         </button>
 
         <span v-if="order.some(item => meal.name in item)" class="font-semibold">{{order.find(item => meal.name in
-            item)[meal.name] }}</span>
+            item)[meal.name].count}}</span>
+        <span v-else-if="showBorder" class="font-semibold">0</span>
 
-        <button @click="increaseOrder(meal.name)"
-            class="bg-[#EE2737] rounded-full w-8 h-8 flex items-center justify-center">
+        <button v-if="showBorder || !order.some(item => meal.name in item)"
+            @click="increaseOrder(meal.name, meal.price, meal.name); addClicked()"
+            class="bg-[#EE2737] rounded-full w-full h-full flex items-center justify-center">
             <font-awesome icon="fa-solid fa-plus" style="color: #ffffff;" />
         </button>
     </div>
@@ -21,6 +24,25 @@ const { increaseOrder, decreaseOrder } = useOrder()
 
 const { meal } = defineProps(['meal'])
 const order = useState('order', () => [])
+
+const showBorder = ref(false)
+let time
+let delay = 2500
+const addClicked = () => {
+    showBorder.value = true
+    clearTimeout(time)
+    time = setTimeout(() => {
+        showBorder.value = false
+    }, delay);
+}
+
+const countClicked = () => {
+    showBorder.value = true
+    clearTimeout(time)
+    time = setTimeout(() => {
+        showBorder.value = false
+    }, delay);
+}
 </script>
 
 <style lang="scss" scoped></style>
--- composables/useOrder.ts
+++ composables/useOrder.ts
@@ -1,21 +1,36 @@
-type OrderItem = { [key: string]: number };
+type OrderItem = { 
+  [key: string]: {
+    count:number,
+    price:number,
+    name:string
+  } 
+};
+
 export const useOrder = () => {
   const order = useState<OrderItem[]>('order', () => []);
 
-  const increaseOrder = (meal: string) => {
+  const increaseOrder = (meal: string, price:number, name:string) => {
     const item = order.value.find((item) => meal in item);
     if (item) {
-      item[meal] += 1;
+      item[meal].count += 1;
     } else {
-      order.value.push({ [meal]: 1 });
+      order.value.push({ 
+        [meal]: {
+          count:1,
+          price,
+          name
+      } 
+    });
     }
+    console.log(order.value);
+    
   };
 
   const decreaseOrder = (meal: string) => {
     const item = order.value.find((item) => meal in item);
     if (item) {
-      item[meal] -= 1;
-      if (item[meal] === 0) {
+      item[meal].count -= 1;
+      if (item[meal].count === 0) {
         order.value.splice(order.value.indexOf(item), 1); // Fix incorrect removal
       }
     }
