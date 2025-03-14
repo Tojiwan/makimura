ff53fdc0be2aea3345b3b41344363cae354f44ee
REYVIC-DEV <joshuavreyes.dev@gmail.com>
2025-03-14 13:47:22 +0800

    added padding to index when cart visible

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div :class="{ 'pb-20':count > 0}">
 		<BranchDelivery />
 		<hotselling />
 		<product :id="category.slug" v-for="category in categoryMeals" :category="category" />
@@ -8,6 +8,10 @@
 </template>
 
 <script setup>
+const order = useState('order', () => []);
+const count = computed(() => {
+    return Object.values(order.value).reduce((sum, item) => sum + (item.count || 0), 0);
+});
 
 const categoryMeals = useState('categoryMeals', () => { });
 
