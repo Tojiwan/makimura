ef083f8917c210394dd0f249fad9945d7728a26c
REYVIC-DEV <joshuavreyes.dev@gmail.com>
2025-03-14 16:55:22 +0800

    search component added

---
+++ components/SearchFilter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="sticky top-10 z-20 flex w-full">
+		<button class="ml-auto bg-[#EE2737] text-white rounded-md p-2 w-[35px] h-[35px] flex items-center justify-center" @click="BDMenuOpen = !BDMenuOpen"><font-awesome icon="fas location-dot" /></button>
+    </div>
+</template>
+
+<script setup>
+const BDMenuOpen = useState('BDMenuOpen', () => true)
+
+</script>
+
+<style scoped>
+
+</style>
