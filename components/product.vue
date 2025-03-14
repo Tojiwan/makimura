9985bd6da1c2d632043ba7a0ba3de9d2a04dd22a
Tojiwan <hithug9909@gmail.com>
2025-03-14 08:03:24 +0800

    asd

--- components/product.vue
+++ components/product.vue
@@ -3,13 +3,13 @@
 		<h1 class="font-semibold text-xl mb-4">Party Platter</h1>
 		<div class="rounded-lg">
 			<div class="flex flex-col space-y-4">
-				<div v-for="(item, index) in items" :key="index" class="bg-white p-4 flex items-start rounded-lg">
-					<img class="w-[150px] h-[150px] rounded-[20px] object-contain" src="/public/6762c39f9d19d.png" alt="">
+				<div v-for="(item, index) in items" :key="index" class="bg-white p-4 flex items-center rounded-lg">
+					<img class="w-[120px] h-[120px] rounded-[20px] object-contain" src="/public/6762c39f9d19d.png" alt="">
 
 					<div class="flex flex-col flex-grow justify-between ml-4 h-full truncate">
-						<div>
+						<div class="space-y-2">
 							<p class="mt-2 font-medium">Item Name</p>
-							<p class="font-bold text-sm">Descriptionasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdadasdasdasdasdas</p>
+							<p class="font-bold text-sm font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quibusdam obcaecati natus maiores labore rem? Ex quia assumenda sunt quidem veritatis, quam cumque necessitatibus modi aliquam pariatur quod eos minus?</p>
 							<p class="font-bold text-sm">1202</p>
 						</div>
 
