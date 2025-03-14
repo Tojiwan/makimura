2cdc9bb684ed9e0e35673231c4c7d35d01158277
REYVIC-DEV <joshuavreyes.dev@gmail.com>
2025-03-14 16:20:56 +0800

    fixed layouts added change branch button

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 	<div class="p-6 bg-white flex flex-col items-center justify-center" :class="{ 'pb-20':count > 0}">
 		<BranchDelivery />
 		<h1 class="tf-bebas text-4xl font-bold -tracking-[4px]">FROM OUR MENU</h1>
+		<button class="" @click="BDMenuOpen = !BDMenuOpen">Change Branch</button>
 		<hotselling />
 		<product :id="category.slug" v-for="category in categoryMeals" :category="category" />
 		<Cart />
@@ -9,6 +10,7 @@
 </template>
 
 <script setup>
+const BDMenuOpen = useState('BDMenuOpen', () => true)
 const order = useState('order', () => []);
 const count = computed(() => {
     return Object.values(order.value).reduce((sum, item) => sum + (item.count || 0), 0);
