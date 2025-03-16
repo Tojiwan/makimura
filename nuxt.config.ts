fe5b6748c96ed9464cd01ff59df03f47f8fdf01e
Tojiwan <hithug9909@gmail.com>
2025-03-16 14:52:59 +0800

    Payment Method UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,8 @@ export default defineNuxtConfig({
   fontawesome: {
     icons: {
       solid: ['dollar-sign', 'cog', 'circle', 'check', 'calendar', 'fire', 'plus', 'minus', 'x', 'location-dot', 'chevron-right', 'chevron-down', 'chevron-up', 'building-columns', 'credit-card'],
-      regular: ['user']
+      regular: ['user', 'money-bill-1'],
+      brands: ['cc-visa', 'cc-mastercard']
     }
   },
 
