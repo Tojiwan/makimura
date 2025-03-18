54b89d4a24bad6dfa503059b6b573c7e58ca7995
Tojiwan <hithug9909@gmail.com>
2025-03-18 15:53:51 +0800

    Updated Payment Page

--- pages/payment.vue
+++ pages/payment.vue
@@ -1,25 +1,21 @@
 <template>
 	<div>
 		<Delivery v-if="isSmallScreen" />
-		<NavBar v-else />
-		
-		<div>
-			<div v-if="isSmallScreen">
-				<SmallScreenComponent />
-			</div>
+		<NavBar />
+		<PaymentBanner />
 
-			<div v-else>
-				<div class="md:flex md:justify-between md:gap-6 p-5 md:p-10 lg:flex lg:justify-between lg:gap-6 p-5 lg:p-10">
-					<div class="lg:w-full md:w-full space-y-6">
-						<BillingAddress />
-						<OtherShipping />
-					</div>
+		<div v-if="!isSmallScreen">
+			<div
+				class="md:flex md:justify-between md:gap-6 p-5 md:p-10 lg:flex lg:justify-between lg:gap-6 p-5 lg:p-10">
+				<div class="lg:w-full md:w-full space-y-6">
+					<BillingAddress />
+					<OtherShipping />
+				</div>
 
-					<div class="lg:w-full md:w-full space-y-6">
-						<YourOrder />
-						<OrderTotal />
-						<PaymentMethod />
-					</div>
+				<div class="lg:w-full md:w-full space-y-6">
+					<YourOrder />
+					<OrderTotal />
+					<PaymentMethod />
 				</div>
 			</div>
 		</div>
@@ -28,20 +24,20 @@
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
-const isSmallScreen = ref(false)
+const isSmallScreen = useMediaQuery('(max-width: 768px)')
 
-const handleResize = () => {
-	if (typeof window !== 'undefined') {
-		isSmallScreen.value = window.innerWidth < 1024
-	}
-}
+// const handleResize = () => {
+// 	if (typeof window !== 'undefined') {
+// 		isSmallScreen.value = window.innerWidth < 1024
+// 	}
+// }
 
-onMounted(() => {
-	handleResize()
-	window.addEventListener('resize', handleResize)
-})
+// onMounted(() => {
+// 	handleResize()
+// 	window.addEventListener('resize', handleResize)
+// })
 
-onUnmounted(() => {
-	window.removeEventListener('resize', handleResize)
-})
+// onUnmounted(() => {
+// 	window.removeEventListener('resize', handleResize)
+// })
 </script>
