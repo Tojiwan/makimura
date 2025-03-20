cbad8501a1b89b7e5134047831bdac46ea5c0d42
REYVIC-DEV <joshuavreyes.dev@gmail.com>
2025-03-20 10:41:50 +0800

    2nd v

--- pages/edit/[meal].vue
+++ pages/edit/[meal].vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col">
-        <button @click="navigateTo('/payment')" class=" absolute top-4 left-4 z-10 bg-main text-white rounded-full w-10 h-10 flex items-center justify-center
-            shadow-lg">
+        <button class=" absolute top-4 left-4 z-10 bg-main text-white rounded-full w-10 h-10 flex items-center justify-center
+            shadow-lg" @click="navigateTo('/payment')">
             <font-awesome :icon="['fas', 'x']" />
         </button>
         <img class="border-b object-cover h-40 w-full" :src="meal_details.image_small" alt="">
@@ -16,19 +16,19 @@
                 <h1 class="font-bold">Note to Makimura</h1>
                 <p class="rounded-full bg-gray-200 px-2">Optional</p>
             </div>
-            <textarea name="" id=""
+            <textarea id="" name=""
                 class="w-full border rounded-lg p-3 outline-0 focus:ring focus:ring-1 ring-[#00b14f] focus:border-[#00b14f]"
-                placeholder="Add your request (subject to Makimura's discretion)"></textarea>
+                placeholder="Add your request (subject to Makimura's discretion)"/>
             <div class="w-full flex items-center justify-center">
                 <div class="rounded-full flex items-center justify-between h-8 w-[150px]">
-                    <button @click.stop="count > 0 ? count-- : count = count"
-                        class="bg-main/70 rounded-full w-8 h-8 flex items-center justify-center">
+                    <button class="bg-main/70 rounded-full w-8 h-8 flex items-center justify-center"
+                        @click.stop="count > 0 ? count-- : count = count">
                         <font-awesome icon="fa-solid fa-minus" />
                     </button>
 
                     <span class="font-semibold">{{ count }}</span>
 
-                    <button @click.stop="count++" class="bg-main rounded-full w-8 h-8 flex items-center justify-center">
+                    <button class="bg-main rounded-full w-8 h-8 flex items-center justify-center" @click.stop="count++">
                         <font-awesome icon="fa-solid fa-plus" style="color: #ffffff;" />
                     </button>
                 </div>
@@ -38,10 +38,10 @@
         <!-- remove/update button -->
         <div
             class="flex items-center justify-center p-3 px-5 absolute bottom-0 w-full bg-white border rounded-t-2xl shadow-[rgba(0,0,0,0.2)_0px_0px_10px_1px]">
-            <button @click="remove_order" v-if="count === 0"
-                class="p-3 w-full rounded-full text-white font-bold bg-red-500">Remove</button>
-            <button @click="update_order" v-else
-                class="p-3 w-full rounded-full text-white font-bold bg-[#1FA530]">Update Item</button>
+            <button v-if="count === 0" class="p-3 w-full rounded-full text-white font-bold bg-red-500"
+                @click="remove_order">Remove</button>
+            <button v-else class="p-3 w-full rounded-full text-white font-bold bg-[#1FA530]"
+                @click="update_order">Update Item</button>
         </div>
     </div>
 </template>
